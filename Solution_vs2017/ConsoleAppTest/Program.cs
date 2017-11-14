7e564916c8bc5bff1b56d1ae7c0b4bbd14e32c35
yzz <1277745281@qq.com>
2017-11-14 21:36:11 +0800

    -

---
+++ Solution_vs2017/ConsoleAppTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleAppTest.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"测试中...");
+			new ThreadPoolTest().Do();
+			
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Solution_vs2017/ConsoleAppTest/Test/ThreadPoolTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest.Test
+{
+	class ThreadPoolTest
+	{
+		public void Do()
+		{
+			ThreadPool.QueueUserWorkItem(o =>
+			{
+				while (true)
+				{
+					Console.WriteLine($"线程运行中...  {DateTime.Now.ToStr()}");
+
+					Thread.Sleep(1);
+					if (true)
+					{
+						continue;
+					}
+				}
+			});
+		}
+	}
+}

20bd177ca228ec9c94f7c7a877c42d5d40424075
yzz <1277745281@qq.com>
2017-11-30 23:04:39 +0800

    -

--- Solution_vs2017/WebTest/res/js/layui/layuiConfig.js
+++ Solution_vs2017/WebTest/res/js/layui/layuiConfig.js
@@ -1,3 +1,4 @@
 ﻿layui.config({
-	base: '/res/js/layui/modules/' //（自定义模块目录，避免过多js引用，页面显得更简洁）你存放新模块的目录，注意，不是layui的模块目录
+	base: '/res/js/layui/modules/' //（自定义模块目录，避免过多js引用，页面显得更简洁）
+	//你存放新模块的目录，注意，不是layui的模块目录
 })//.use('demo'); //加载入口(非必要)
--- Solution_vs2017/WebTest/res/js/layui/modules/demo.js
+++ Solution_vs2017/WebTest/res/js/layui/modules/demo.js
@@ -13,17 +13,17 @@
 
 
 //定义一个模块，第一个参数可以加上 [] 中括号，表示模块加载依赖
-layui.define(['layer'], function (exports) {
+layui.define(function (exports) {
 	//do something
 	console.log("demo加载完成"); //加载完成的处理
 
-	//1、定义一个模块，该exports的个数必须唯一
+	//1、定义一个模块，该exports的个数必须只有一个
 	//2、并且模块名称、文件名称必须一致
-	//3、exports第二个参数可以使对象，也可以是方法
+	//3、exports第二个参数可以是对象，也可以是方法
 	exports('demo', {
 		//自定义模块对象内部方法
-		func: function () {
-			console.log("demo.func()");
+		func: function (index) {
+			console.log("demo.func(" + index+")");
 		}
 	});
 

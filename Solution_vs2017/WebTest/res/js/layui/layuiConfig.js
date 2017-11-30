258707ea9ad6a5ca8d6d336f00d5e735b36ffc77
yzz <1277745281@qq.com>
2017-11-30 18:14:46 +0800

    -

---
+++ Solution_vs2017/WebTest/res/js/layui/layuiConfig.js
@@ -0,0 +1,3 @@
+﻿layui.config({
+	base: '/res/js/layui/modules/' //（自定义模块目录）你存放新模块的目录，注意，不是layui的模块目录
+})//.use('demo'); //加载入口(非必要)
---
+++ Solution_vs2017/WebTest/res/js/layui/modules/demo.js
@@ -0,0 +1,36 @@
+﻿//layui.define(['layer', 'form'], function (exports) {
+//	var layer = layui.layer
+//		, form = layui.form;
+
+//	layer.msg('Hello World');
+
+//	console.log(1);
+
+//	exports('index',{}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
+//});  
+
+
+
+
+//定义一个模块，第一个参数可以加上 [] 中括号，表示模块加载依赖
+layui.define(['layer'], function (exports) {
+	//do something
+	console.log("demo加载完成"); //加载完成的处理
+
+	//1、定义一个模块，该exports的个数必须唯一
+	//2、并且模块名称、文件名称必须一致
+	//3、exports第二个参数可以使对象，也可以是方法
+	exports('demo', {
+		//自定义模块对象内部方法
+		func: function () {
+			console.log("demo.func()");
+		}
+	});
+
+
+
+	//第二个没用
+	//exports('demo2', function () {
+	//	console.log("demo2()");
+	//});
+});

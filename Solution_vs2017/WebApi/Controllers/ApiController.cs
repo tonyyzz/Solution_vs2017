8d0cff2c7e83bd838c77172fdcb248456a3ac34d
yzz <1277745281@qq.com>
2017-12-11 14:45:31 +0800

    -

---
+++ Solution_vs2017/WebApi/Controllers/ApiController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApi.Controllers
+{
+	public class ApiController : Controller
+	{
+		// GET: Api
+		public ActionResult list()
+		{
+			string pageStr = Request["page"] ?? "";
+			int page = 0;
+			int.TryParse(pageStr, out page);
+			if (page <= 0)
+			{
+				page = 1;
+			}
+
+			int pageSize = 10;
+			int totalCount = GetList().Count();
+			int pageCount = Convert.ToInt32(Math.Ceiling(totalCount * 1.0 / pageSize));
+
+			var data = GetList().Skip(pageSize * (page - 1)).Take(pageSize).ToList();
+			return Json(new { data = data, pages = pageCount }, JsonRequestBehavior.AllowGet);
+
+
+		}
+
+		public List<int> GetList()
+		{
+			List<int> list = new List<int>();
+			for (int i = 1; i <= 100; i++)
+			{
+				list.Add(i);
+			}
+			return list;
+		}
+	}
+}

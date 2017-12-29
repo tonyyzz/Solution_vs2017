77f7480ed56a1b5b12b326ba7f43a740778d5f22
yzz <1277745281@qq.com>
2017-12-30 00:07:00 +0800

    -

---
+++ Solution_vs2017/ConsoleAppTest/tsqlTest/PersonModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace ConsoleAppTest.Model
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// 由代码生成器生成，Created Time：2017-12-29 23:03:57
+	public partial class PersonModel
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public PersonModel()
+		{
+			Id = 0;
+			Name = "";
+			Age = 0;
+			Height = 0;
+			Weight = 0;
+		}
+		/// <summary>
+		/// Id
+		/// </summary>
+		public int Id { get; set; }
+		/// <summary>
+		/// Name
+		/// </summary>
+		public string Name { get; set; }
+		/// <summary>
+		/// Age
+		/// </summary>
+		public int Age { get; set; }
+		/// <summary>
+		/// Height
+		/// </summary>
+		public int Height { get; set; }
+		/// <summary>
+		/// Weight
+		/// </summary>
+		public float Weight { get; set; }
+	}
+}
---
+++ Solution_vs2017/ConsoleAppTest/tsqlTest/TsqlTest.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleAppTest.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest.Test
+{
+	class TsqlTest
+	{
+		public void Do()
+		{
+			//预插入1000条数据
+			Random r = new Random();
+			List<PersonModel> pList = new List<PersonModel>();
+			for (int i = 1; i <= 1000; i++)
+			{
+				pList.Add(new PersonModel()
+				{
+					//Id = i,
+					Name = "name" + i,
+					Age = 15 + r.Next(20),
+					Height = 160 + r.Next(20),
+					Weight = 60 + r.Next(20)
+				});
+			}
+			var flag= DAL.BaseDAL.BatchInsert(pList);
+			Console.WriteLine($"批量插入状态：{flag}");
+		}
+	}
+}

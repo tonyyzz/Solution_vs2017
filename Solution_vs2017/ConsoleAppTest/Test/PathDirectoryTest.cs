3d92793537995bfcd607f9fa965da451eb264869
yzz <1277745281@qq.com>
2018-01-15 18:09:52 +0800

    -

--- Solution_vs2017/ConsoleAppTest/Test/PathDirectoryTest.cs
+++ Solution_vs2017/ConsoleAppTest/Test/PathDirectoryTest.cs
@@ -12,14 +12,14 @@ namespace ConsoleAppTest.Test
 		public void Do()
 		{
 			string path = @"C:\Users\Administrator\Desktop\fileTest\test.txt";
-			 var charStr= Path.AltDirectorySeparatorChar;
+			var charStr = Path.AltDirectorySeparatorChar;
 			//var path= Path.ChangeExtension(@"C:\Users\Administrator\Desktop\fileTest\test.txt", null);
-			var newPath= Path.Combine(@"C:\Users", "a");
-			var directory= Path.GetDirectoryName(@"C:\Users\Administrator\Desktop\fileTest\test.txt");
+			var newPath = Path.Combine(@"C:\Users", "a");
+			var directory = Path.GetDirectoryName(@"C:\Users\Administrator\Desktop\fileTest\test.txt");
 			var chs = Path.GetInvalidPathChars();
-			 var randomName= Path.GetRandomFileName();
-			 var tempFileName= Path.GetTempFileName();
-			 var strList=  Directory.EnumerateFiles(@"C:\Users\Administrator\Desktop\fileTest\test.txt");
+			var randomName = Path.GetRandomFileName();
+			var tempFileName = Path.GetTempFileName();
+			var strList = Directory.EnumerateFiles(@"C:\Users\Administrator\Desktop\fileTest\test.txt");
 		}
 	}
 }

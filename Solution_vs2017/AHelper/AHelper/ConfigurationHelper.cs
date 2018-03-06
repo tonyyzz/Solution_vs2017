f809eb9da94c963d1839bfe42c2d8435b8cb60d3
tonyyzz <1277745281@qq.com>
2018-03-06 08:58:15 +0800

    -

---
+++ Solution_vs2017/AHelper/AHelper/ConfigurationHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+	/// <summary>
+	/// 配置文件帮助类
+	/// </summary>
+	public class ConfigurationHelper
+	{
+		/// <summary>
+		/// 获取 ConnectionString value
+		/// </summary>
+		/// <param name="str"></param>
+		/// <returns></returns>
+		public static string GetConnStr(string str)
+		{
+			return ConfigurationManager.ConnectionStrings[str] != null ? ConfigurationManager.ConnectionStrings[str].ToString() : "";
+		}
+
+		/// <summary>
+		/// 获取 AppSetting value
+		/// </summary>
+		/// <param name="str"></param>
+		/// <returns></returns>
+		public static string GetAppSettingStr(string str)
+		{
+			return ConfigurationManager.AppSettings[str] ?? "";
+		}
+	}
+}
